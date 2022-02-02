b6b087ce9d45d0ebd1287695eee918f5ef25c64e
ArtemLytvynenkoa <chpart355@gmail.com>
2022-02-02 15:40:03 +0200

    adds audit to componentDidMount

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
 
   componentDidMount() {
     this.setState({
-      contacts: JSON.parse(localStorage.getItem("contacts"))
+      contacts: JSON.parse(localStorage.getItem("contacts")) ?? []
     })
   }
 
